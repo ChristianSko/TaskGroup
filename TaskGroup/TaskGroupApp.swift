0ef78a19fbc3539e2dad233a3c243bbe2a469bfe
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-28 10:15:29 +0200

    Initial Commit

---
+++ TaskGroup/TaskGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskGroupApp.swift
+//  TaskGroup
+//
+//  Created by Skorobogatow, Christian on 28/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
